3dc7119897e196f9be4028fa3a480472c4eaf72e
youngyin <57608725+youngyin@users.noreply.github.com>
2025-05-30 07:44:56 +0900

    [feat] init project

---
+++ splitmart/src/main/java/com/splitmart/SplitmartApplication.java
@@ -0,0 +1,13 @@
+package com.splitmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitmartApplication.class, args);
+	}
+
+}
---
+++ splitmart/src/test/java/com/splitmart/SplitmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
